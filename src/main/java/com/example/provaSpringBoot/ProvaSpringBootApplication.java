2fec7482b5912ce23425f099226f2b5a8a1635db
Diego <92130153+DiegoPlaninscheck@users.noreply.github.com>
2023-04-20 14:30:13 -0300

    Initial commit

---
+++ src/main/java/com/example/provaSpringBoot/ProvaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.example.provaSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/provaSpringBoot/ProvaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provaSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
